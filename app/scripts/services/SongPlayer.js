26122778409a888e848905aa7da735668e23f4eb
christopherbrennan <chris.ryan.bren@gmail.com>
2017-05-23 15:07:55 -0400

    assignment-7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,6 +3,8 @@
          
          var SongPlayer = {};
          
+         //@desc holds current song number
+         //@type {Number}
          var currentSong = null;
     
          //@desc Buzz object audio file
@@ -27,21 +29,34 @@
              currentSong = song;
          };
          
+         //@function playSong
+         //@desc plays current song and sets song.playing to true
+         //@param {Object} song
+         var playSong = function(song) {
+             currentBuzzObject.play();
+             song.playing = true;
+               
+         };
+         
+         //@function SongPlayer.play
+         //@desc play current song if paused, or selected song
+         //@param {Object} song
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
                  
                  setSong(song);
-                 currentBuzzObject.play();
-                 song.playing = true;
+                 playSong(song);
                  
              } else if (currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
-                     currentBuzzObject.play();
-                     song.playing = true;
+                     playSong(song);
                  }
              }
          };
          
+         //@function SongPlayer.pause
+         //@desc Pauses current song
+         //@param {Object}
          SongPlayer.pause = function(song) {
              currentBuzzObject.pause();
              song.playing = false;
