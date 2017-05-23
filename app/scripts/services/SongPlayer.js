555f62a36361568ecfc8c3d130a9ea62ab84e03b
christopherbrennan <chris.ryan.bren@gmail.com>
2017-05-23 17:13:41 -0400

    checkpoint-8

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,11 +1,9 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
          
          var SongPlayer = {};
          
-         //@desc holds current song number
-         //@type {Number}
-         var currentSong = null;
+         var currentAlbum = Fixtures.getAlbum();
     
          //@desc Buzz object audio file
          //@type {Object}
@@ -18,7 +16,7 @@
          var setSong = function(song) {
              if (currentBuzzObject) {
                  currentBuzzObject.stop();
-                 currentSong.playing = null;
+                 SongPlayer.currentSong.playing = null;
              }
              
              currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -26,8 +24,18 @@
                  preload: true
              });
              
-             currentSong = song;
+             SongPlayer.currentSong = song;
          };
+         
+         //@desc activate song object from list of songs
+         //@type {Object}
+         var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+         };
+         
+         //@desc holds current song number
+         //@type {Number}
+         SongPlayer.currentSong = null;
          
          //@function playSong
          //@desc plays current song and sets song.playing to true
@@ -42,12 +50,11 @@
          //@desc play current song if paused, or selected song
          //@param {Object} song
          SongPlayer.play = function(song) {
-             if (currentSong !== song) {
-                 
+             song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
                  setSong(song);
                  playSong(song);
-                 
-             } else if (currentSong === song) {
+             } else if (SongPlayer.currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
                      playSong(song);
                  }
@@ -58,8 +65,26 @@
          //@desc Pauses current song
          //@param {Object}
          SongPlayer.pause = function(song) {
+             song = song || SongPlayer.currentSong;
              currentBuzzObject.pause();
              song.playing = false;
+         };
+         
+         //@function SongPlayer.previous
+         //@desc retrieves previous song
+         //@param {Object} 
+         SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+             
+            if (currentSongIndex < 0) {
+            currentBuzzObject.stop();
+            SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+     }
          };
          
          return SongPlayer;
@@ -67,5 +92,5 @@
      
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['Fixtures', SongPlayer]);
  })();
